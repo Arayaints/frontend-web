9f2b8c44d9b1201408032d6b368bbef47a3b8825
Arayaints <araya.ints@gmail.com>
2026-02-22 19:44:18 +0700

    test

--- frontend-web/src/app/layout/main-layout.component/main-layout.component.ts
+++ frontend-web/src/app/layout/main-layout.component/main-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
-import { HeaderComponent } from '../header.component/header.component';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout.component',
-  imports: [HeaderComponent,RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
